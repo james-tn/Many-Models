188417d483439e2cc7fe13d526609bd740883b26
james-tn <james.nguyen@microsoft.com>
2022-09-21 12:46:56 -0700

    add v2

--- code/deployment/ray_many_models/script/aml_score.py
+++ code/deployment/ray_many_models/script/aml_score.py
@@ -5,26 +5,27 @@ import mlflow
 # Called when the service is loaded
 # import time
 import requests
-# from azure.identity import ManagedIdentityCredential 
-# from azure.ai.ml import MLClient
+from azure.identity import ManagedIdentityCredential 
+from azure.ai.ml import MLClient
 
 def init():
     # global ws, handle
 
-    pass
 
+    resource_group = "azureml"
 
-    # credential = ManagedIdentityCredential()
-
-    # ml_client = MLClient(credential=credential,subscription_id=subscription_id,resource_group_name=resource_group, workspace_name=workspace_name)
-    # model_operations = ml_client.models
-    # print("return model" , model_operations.get("sklearn-iris", version=1))
+    subscription_id = "0e9bace8-7a81-4922-83b5-d995ff706507"
+    credential = ManagedIdentityCredential()
+    workspace_name = "ws01ent"
+    ml_client = MLClient(credential=credential,subscription_id=subscription_id,resource_group_name=resource_group, workspace_name=workspace_name)
+    model_operations = ml_client.models
+    print("return model" , model_operations.get("sklearn-iris", version=1))
 
 def run(raw_data):
         # Get the input data 
     text_input = json.loads(raw_data)
     # output = ray.get(handle.remote(text_input))
-    response = requests.post('http://rayservice-sample-serve-svc.default.svc.cluster.local:8000/', json=text_input)
+    response = requests.post('http://many-models-serve-svc.default.svc.cluster.local:8000/', json=text_input)
 
     output = response.text
     return json.dumps({"result":output})
